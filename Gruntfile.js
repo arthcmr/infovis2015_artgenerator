ad9dd6e5131f42632961310de91c019cc3332a77
Arthur Camara <arthurcamara@gmail.com>
2015-02-23 20:38:56 +0100

    Copy all assets

--- Gruntfile.js
+++ Gruntfile.js
@@ -130,10 +130,8 @@ module.exports = function(grunt) {
                     dest: '<%= config.dist %>',
                     src: [
                         '*.{ico,png,txt}',
-                        'assets/{,*/}*.{png,gif,jpg}',
                         '{,*/}*.html',
-                        'assets/styles/{,*/}*.css',
-                        'assets/fonts/{,*/}*.*'
+                        'assets/**/*'
                     ]
                 }]
             },
